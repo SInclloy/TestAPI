7fa6330520a9407ba157e48377c46f3a053ecdb2
Tony 7 <messer_git@mail.ru>
2022-08-25 11:58:49 +0300

    set __goto404 to var

--- TestApi/API/Libraries/Tags/TagsByToDoId.py
+++ TestApi/API/Libraries/Tags/TagsByToDoId.py
@@ -7,21 +7,21 @@ from ...models import ToDo, Tag
 from ...serializers import TagSerializer
 
 class TagByToDoId(APIView):
-    def __goto404(self):
-        print("goto404")
-        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    __goto404= Response(status=status.HTTP_404_NOT_FOUND)
+
     def get(self, request, id):
 
         try:todo = ToDo.objects.get(id=id)
         except:
-            return self.__goto404()
+            return self.__goto404
 
         if todo.host != request.user:
-            return self.__goto404()
+            return self.__goto404
         tag = Tag.objects.filter(notes=todo)
 
         if tag==None:
-            return self.__goto404()
+            return self.__goto404
 
         queryset = TagSerializer(tag, many=True)
         return JsonResponse(queryset.data, safe=False)
--- TestApi/API/Libraries/ToDo/ToDoByTagId.py
+++ TestApi/API/Libraries/ToDo/ToDoByTagId.py
@@ -8,20 +8,20 @@ from ...serializers import ToDoSerializer
 
 class ToDoByTagId(APIView):
 
-    def __goto404(self):
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    __goto404 = Response(status=status.HTTP_404_NOT_FOUND)
+
     def get(self, request, id):
 
         try:tag = Tag.objects.get(id=id)
         except:
-            return self.__goto404()
+            return self.__goto404
 
         if tag.host != request.user:
-            return self.__goto404()
+            return self.__goto404
         title = tag.notes
 
         if title == None:
-            return self.__goto404()
+            return self.__goto404
 
         todo = ToDo.objects.filter(title=title)
 
