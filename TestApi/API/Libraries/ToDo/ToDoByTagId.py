7d7cb71eb295d6de7e2d944687ebb4a12783148e
Tony 7 <messer_git@mail.ru>
2022-08-25 11:33:14 +0300

    change __goto404 to def of class

--- TestApi/API/Libraries/ToDo/ToDoByTagId.py
+++ TestApi/API/Libraries/ToDo/ToDoByTagId.py
@@ -8,23 +8,22 @@ from ...serializers import ToDoSerializer
 
 class ToDoByTagId(APIView):
 
+    def __goto404(self):
+        return Response(status=status.HTTP_404_NOT_FOUND)
     def get(self, request, id):
-        def __goto404():
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
 
         try:tag = Tag.objects.get(id=id)
         except:
-            return __goto404()
+            return self.__goto404()
 
         if tag.host != request.user:
-            return __goto404()
+            return self.__goto404()
         title = tag.notes
 
         if title == None:
-            return __goto404()
+            return self.__goto404()
 
         todo = ToDo.objects.filter(title=title)
-        queryset = ToDoSerializer(todo, many=True)
 
+        queryset = ToDoSerializer(todo, many=True)
         return JsonResponse(queryset.data, safe=False)
